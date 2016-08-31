691db32ed56d38ea449e5e6be955b548210d179a
Taras Chornenkiy <taraschrt06@gmail.com>
2016-08-31 13:03:37 +0300

    gitignore added

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
+#include "stm32l152xe.h"
+
+
+
+void delay(void)
+{
+  uint32_t del = 0x80000;
+  
+  while(del--);
+}
 
 int main(void)
 {
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN_Msk;
+  GPIOA->MODER |= 1 << 10;
+  GPIOA->ODR |= 1 << 5;
+  /* Forever loop */
+  for(;;)
+  {
+    GPIOA->ODR ^= 1 << 5;
+    delay();
+  } 
+    
+    
   return 0;
 }
